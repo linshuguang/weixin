e300068add112b84eb41092241eb49d04709b5ff
linshuguang <jinjunjiaotou@gmail.com>
2017-12-05 11:24:05 +0800

    refactor

--- src/main/java/com/me/servlets/MySupport.java
+++ src/main/java/com/me/servlets/MySupport.java
@@ -5,6 +5,7 @@ import com.github.sd4324530.fastweixin.message.BaseMsg;
 import com.github.sd4324530.fastweixin.message.TextMsg;
 import com.github.sd4324530.fastweixin.message.req.TextReqMsg;
 import com.github.sd4324530.fastweixin.servlet.WeixinSupport;
+import org.codehaus.plexus.util.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,29 +20,30 @@ import java.util.Properties;
 //@PropertySource("classpath:config/config.properties")
 //@Configuration
 public class MySupport extends WeixinSupport {
-    private static final Logger log = LoggerFactory.getLogger(MySupport.class);
+    private static final Logger logger = LoggerFactory.getLogger(MySupport.class);
 
     //@Value("${weixin.appId}")
     private String appId;
 
+    private final String filename = "config/config.properties";
+    private final String appIdConfName = "weixin.appId";
 
     @Override
     protected String getToken(){
         Properties prop = new Properties();
         InputStream input = null;
         try {
-            String filename = "config/config.properties";
             input = MySupport.class.getClassLoader().getResourceAsStream(filename);
-            if(input!=null){
-                prop.load(input);
-                appId = prop.getProperty("weixin.appId");
-            }
+            prop.load(input);
+            appId = prop.getProperty(appIdConfName);
         }catch (Exception e) {
+            logger.error(ExceptionUtils.getFullStackTrace(e));
         } finally {
             if (input != null) {
                 try {
                     input.close();
                 } catch (IOException e) {
+                    logger.error(ExceptionUtils.getFullStackTrace(e));
                 }
             }
         }
@@ -51,7 +53,7 @@ public class MySupport extends WeixinSupport {
     @Override
     protected BaseMsg handleTextMsg(TextReqMsg msg) {
         String content = msg.getContent();
-        log.debug("用户发送到服务器的内容:{}", content);
+        logger.debug("用户发送到服务器的内容:{}", content);
         return new TextMsg("服务器回复用户消息!");
     }
 
