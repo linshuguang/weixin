d811bd8d9f016ca45d9ebd1939757ffebd64dec6
linshuguang <jinjunjiaotou@gmail.com>
2017-12-05 11:17:41 +0800

    read appid

--- src/main/java/com/me/servlets/MyServletWeixinSupport.java
+++ src/main/java/com/me/servlets/MyServletWeixinSupport.java
@@ -1,6 +1,9 @@
 package com.me.servlets;
 
+import com.github.sd4324530.fastweixin.util.SignUtil;
+import org.codehaus.plexus.util.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import com.github.sd4324530.fastweixin.message.BaseMsg;
 import com.github.sd4324530.fastweixin.message.TextMsg;
@@ -9,18 +12,47 @@ import com.github.sd4324530.fastweixin.servlet.WeixinSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by kenya on 2017/12/5.
  */
-@PropertySource("classpath:config/config.properties")
+//@PropertySource("classpath:config/config.properties")
+//@Configuration
 public class MyServletWeixinSupport extends WeixinSupport {
     private static final Logger log = LoggerFactory.getLogger(MyServletWeixinSupport.class);
 
-    @Value("${weixin.appId}")
+    //@Value("${weixin.appId}")
     private String appId;
 
+    public void setAppId(String appId){
+        this.appId = appId;
+    }
+
     @Override
-    protected String getToken() {
+    protected String getToken(){
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
+            String filename = "config/config.properties";
+            input = MyServletWeixinSupport.class.getClassLoader().getResourceAsStream(filename);
+            if(input!=null){
+                prop.load(input);
+                appId = prop.getProperty("weixin.appId");
+            }
+        }catch (Exception e) {
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                }
+            }
+        }
         return appId;
     }
 
@@ -31,7 +63,12 @@ public class MyServletWeixinSupport extends WeixinSupport {
         return new TextMsg("服务器回复用户消息!");
     }
 
-
-
+    @Override
+    public boolean isLegal(HttpServletRequest request) {
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        return SignUtil.checkSignature(this.getToken(), signature, timestamp, nonce);
+    }
 
 }
--- src/main/java/com/me/servlets/WeixinServlet.java
+++ src/main/java/com/me/servlets/WeixinServlet.java
@@ -2,20 +2,52 @@ package com.me.servlets;
 
 import com.github.sd4324530.fastweixin.servlet.WeixinServletSupport;
 import com.github.sd4324530.fastweixin.servlet.WeixinSupport;
+import org.codehaus.plexus.util.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Properties;
 
 /**
  * Created by kenya on 2017/12/5.
  */
-public class WeixinServlet extends WeixinServletSupport {
+public class WeixinServlet extends HttpServlet{
+
+    private static final Logger logger = LoggerFactory.getLogger(WeixinServlet.class);
+
+    private final MyServletWeixinSupport support = new MyServletWeixinSupport();
+
+    public WeixinServlet(){
+
+    }
+
 
     @Override
-    protected WeixinSupport getWeixinSupport() {
-        return new MyServletWeixinSupport();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.debug("get");
+
+        getServletContext().log("hello :"+support.getToken());
+        this.support.bindServer(request, response);
     }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.debug("post");
+        if(this.support.isLegal(request)) {
+            response.setCharacterEncoding("UTF-8");
+            String resp = this.support.processRequest(request);
+            PrintWriter pw = response.getWriter();
+            pw.write(resp);
+            pw.flush();
+            pw.close();
+        }
+    }
+
 }
